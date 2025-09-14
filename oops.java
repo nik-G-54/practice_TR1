667d99f0bd00759f22f94ffa436f43a833c21e62
nik-G-54 <nikhilgupta542006@gmail.com>
2025-09-14 23:21:55 +0530

    Add files via upload

---
+++ oops.java
@@ -0,0 +1,17 @@
+import Mypakage1.Myclass;
+
+public class oops {
+    public static void main(String[] args) {
+     Employee emp=new Employee( "hdhvkj",2);
+        System.out.println(emp.salary);
+        System.out.println(emp.countSalary());
+
+        Myclass cl=new Myclass();
+        cl.print12();
+        System.out.println(emp.getph());
+        System.out.println(emp.getname());
+        System.out.println(emp.employeeid);
+          emp.getph("pradeep");
+
+    }
+}
